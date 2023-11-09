9fc1b9b3dbf1ab841d90bc5cb0b7c507d4b25db6
Tomas Emmanuel Albero <tomasemmanuel.albero.cics@ust.edu.ph>
2023-11-09 15:25:04 +0800

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,35 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @php
+                    $i=1
+                    @endphp
+                    @foreach ($users as $user)
+                    <tr>
+                        <th scope="row">{{$i++}}</th>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>{{$user->created_at->diffforhumans()}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</x-app-layout>
